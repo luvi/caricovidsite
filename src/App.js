ec961086d09310c0bea858e211143a2bd90b0d55
Janique-ka John <spjaniqueka@gmail.com>
2020-03-23 22:01:10 +0100

    total cases update

--- src/App.js
+++ src/App.js
@@ -136,7 +136,7 @@ export default class App extends Component {
     });
 
     var popup = new mapboxgl.Popup({ offset: 25 }).setText(
-      "Construction on the Washington Monument began in 1848."
+      "Martinique has X cases"
     );
     // add marker to map
     new mapboxgl.Marker()
