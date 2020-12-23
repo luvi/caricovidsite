77feda11da01394dd3530b0c4b13909e0c68b1ab
Janique-ka John <spjaniqueka@gmail.com>
2020-12-23 10:09:49 +0100

    improve seo

--- src/MAPBOX_ACCESS_TOKEN.js
+++ src/MAPBOX_ACCESS_TOKEN.js
@@ -1,5 +1,5 @@
-export const MAPBOX_ACCESS_TOKEN =
-  "pk.eyJ1IjoibHV2aXNhY2NoYXJpbmUiLCJhIjoiY2s4NjcwbmtsMDJwZjNkbXZobTVhcnZrbiJ9.CfCqBzy5gF2kjwoLvCfTEQ";
+// export const MAPBOX_ACCESS_TOKEN =
+  // "pk.eyJ1IjoibHV2aXNhY2NoYXJpbmUiLCJhIjoiY2s4NjcwbmtsMDJwZjNkbXZobTVhcnZrbiJ9.CfCqBzy5gF2kjwoLvCfTEQ";
 
-
- 
+  export const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoibHV2aXNhY2NoYXJpbmUiLCJhIjoiY2lvZGVtc2ZzMDAwNncxbHhhcDhjYmg0MiJ9.Bv-XTRkC9mhbV-_XOwCoyQ"
+  
