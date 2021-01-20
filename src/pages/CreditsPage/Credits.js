31cfbf4855fdf1bed30fbd9b31d9be9a9fc13b86
maximilian.ungar <maximilian.ungar@klarna.com>
2021-01-20 22:23:43 +0100

    Add i18n support to Credits (#20)
    We change the class component to a function component as we have no
    state to take care of. Also, we can use the i18n hook for our
    translations.
    We use the useEffect hook instead of the componentDidMount
    lifecycle method as with the empty array it has the same effect.

--- src/pages/CreditsPage/Credits.js
+++ src/pages/CreditsPage/Credits.js
@@ -1,58 +1,56 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import { useTranslation } from 'react-i18next'
 
-export default class Credits extends Component {
+const Credits = () => {
+  const { t } = useTranslation()
 
-
-  componentDidMount() {
+  useEffect(() => {
     document.body.style.backgroundColor = "#1A2637";
-  }
+  }, [])
 
-  render() {
-    return (
-      <div>
-        <div
-          style={{
-            marginTop: "100px",
-            display: "flex",
-            flexDirection: "column",
-            position: "relative",
-            color: "white",
-          }}
-          className="sourcesContainer"
-        >
-          <div>
-            Developer:
-            <a href="https://twitter.com/JaniquekaJohn"> @JaniquekaJohn</a>
-          </div>
-
-          <div>
-            Data Sources: Johns Hopkins, @KevzPolitics, Trinidad Express,
-            Dominica News Online
-          </div>
-          <div>
-            <a href="https://github.com/luvi/caricoviddata">
-              Our own open source dataset
-            </a>
-          </div>
-          <div>BVI News</div>
-          <div> </div>
-          <div></div>
-          <div class="disclaimer">
-            Disclaimer: Data shown on this site is for information purposes
-            only, please keep update delays in mind.
-          </div>
+  return (
+    <div>
+      <div
+        style={{
+          marginTop: "100px",
+          display: "flex",
+          flexDirection: "column",
+          position: "relative",
+          color: "white",
+        }}
+        className="sourcesContainer"
+      >
+        <div>
+          {t('developer')}: <a href="https://twitter.com/JaniquekaJohn">{t('twitter_handle')}</a>
         </div>
 
-        <div style={{
-            display: "flex",
-            margin: "auto",
-            flexDirection: "column",
-            position: "relative",
-            color: "white",
-            
-          }}>
+        <div>
+          {t('data_sources')}
+        </div>
+        <div>
+          <a href="https://github.com/luvi/caricoviddata">
+          {t('own_data_set')}
+          </a>
+        </div>
+        <div>{t('bvi_news')}</div>
+        <div> </div>
+        <div></div>
+        <div class="disclaimer">
+          {t('credits_disclaimer')}
         </div>
       </div>
-    );
-  }
+
+      <div style={{
+        display: "flex",
+        margin: "auto",
+        flexDirection: "column",
+        position: "relative",
+        color: "white",
+
+      }}>
+      </div>
+    </div>
+  );
 }
+
+export default Credits
