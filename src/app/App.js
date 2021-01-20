8010300b678483d57cae40c72db5c450656cfd7f
maximilian.ungar <maximilian.ungar@klarna.com>
2021-01-20 22:20:44 +0100

    Add translations to App.js (#20)
    We order the imports based on origin, e.g. packages, css files, local
    files.
    We change the class component into a functional component, because we
    have no state we need to take care of. Also, it enables us to use the
    i18n hook for our translations.

--- src/app/App.js
+++ src/app/App.js
@@ -1,53 +1,56 @@
-  import React, { Component } from "react";
-  import "./App.css";
-  import "bootstrap/dist/css/bootstrap.min.css";
-  import { Navbar, Nav } from "react-bootstrap";
-  import Map from "../pages/MapPage/Map";
-  import Credits from "../pages/CreditsPage/Credits";
-  import GraphPage from "../pages/GraphPage/GraphPage";
-  import TestsPage from '../pages/TestsPage/TestsPage'
+import React, { Component } from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { useTranslation } from 'react-i18next'
+import { Navbar, Nav } from "react-bootstrap"
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-  import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Map from "../pages/MapPage/Map"
+import Credits from "../pages/CreditsPage/Credits"
+import GraphPage from "../pages/GraphPage/GraphPage"
+import TestsPage from '../pages/TestsPage/TestsPage'
 
-  export default class App extends Component {
+export default () => {
+  const {t, i18n} = useTranslation()
 
-    render() {
-      return (
-        <div className="App">
-          <Router>
-            <Navbar className={"header-color"}  variant="dark" expand="lg" fixed="top">
-              <Link to="/">
-                <Navbar.Brand>Caribbean COVID Map</Navbar.Brand>
-              </Link>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="justify-content-end" activeKey="/home">
-                <Nav.Item>
-                    <Nav.Link style={{ color: "white", paddingRight: 10 }} href="/tests">Tests (beta)</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link style={{ color: "white", paddingRight: 10 }} href="/graphs">Case Graphs</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <div> </div>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link style={{ color: "white" }} href="/credits">Credits</Nav.Link>
-                  </Nav.Item>
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
+  return (
+  <div className="App">
+    <Router>
+      <Navbar className={"header-color"}  variant="dark" expand="lg" fixed="top">
+        <Link to="/">
+          <Navbar.Brand>{ t('title') }</Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="justify-content-end" activeKey="/home">
+            <Nav.Item>
+              <Nav.Link style={{ color: "white", paddingRight: 10 }} onClick={() => i18n.changeLanguage('en')}>{t('language_english')}</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link style={{ color: "white", paddingRight: 10 }} href="/tests">{t('tests_title')}</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link style={{ color: "white", paddingRight: 10 }} href="/graphs">{t('case_graphs')}</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <div> </div>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link style={{ color: "white" }} href="/credits">{t('credits')}</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
-            <div>
-              <Switch>
-                <Route exact path="/" component={Map} />
-                <Route path="/credits" component={Credits} />
-                <Route path="/graphs" component={GraphPage} />
-                <Route path="/tests" component={TestsPage} />
-              </Switch>
-            </div>
-          </Router>
-        </div>
-      );
-    }
-  }
+      <div>
+        <Switch>
+          <Route exact path="/" component={Map} />
+          <Route path="/credits" component={Credits} />
+          <Route path="/graphs" component={GraphPage} />
+          <Route path="/tests" component={TestsPage} />
+        </Switch>
+      </div>
+    </Router>
+  </div>
+)
+}
