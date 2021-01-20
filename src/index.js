db51e3620a9ff0a91298074581a635a05579b059
maximilian.ungar <maximilian.ungar@klarna.com>
2021-01-20 22:19:29 +0100

    Import i18n to entrypoint (#20)
    We must load i18n on our entry point so that the object is globally
    available. After this, we cann access the t function with either the
    hook or the hoc.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
+import i18n from '../src/locales'
 
 ReactDOM.render(
   <React.StrictMode>
